739828646e81d37937612492eccd87c4c9f3270f
smoogipoo <smoogipoo@smgi.me>
2019-08-30 18:44:12 +0900

    Fix test scene

--- osu.Game.Tests/Visual/Editor/TestSceneGridLayer.cs
+++ osu.Game.Tests/Visual/Editor/TestSceneGridLayer.cs
@@ -4,24 +4,35 @@
 using System;
 using System.Collections.Generic;
 using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Input.Events;
+using osu.Game.Beatmaps;
+using osu.Game.Beatmaps.ControlPoints;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Osu.Beatmaps;
+using osu.Game.Rulesets.Osu.Objects;
+using osu.Game.Screens.Edit;
 using osu.Game.Screens.Edit.Compose.Components.Grids;
-using osu.Game.Screens.Edit.Compose.Components.Grids.Basic;
 using osuTK;
 using osuTK.Graphics;
 
 namespace osu.Game.Tests.Visual.Editor
 {
-    public class TestSceneGridLayer : OsuTestScene
+    public class TestSceneGridLayer : EditorClockTestScene
     {
         public override IReadOnlyList<Type> RequiredTypes => new[]
         {
             typeof(DrawableGrid),
             typeof(DrawableCircularGrid)
         };
+
+        [Cached(typeof(IEditorBeatmap))]
+        [Cached(typeof(IEditorBeatmap<OsuHitObject>))]
+        private readonly EditorBeatmap<OsuHitObject> beatmap = new EditorBeatmap<OsuHitObject>(new OsuBeatmap());
 
         private GridLayer gridLayer;
         private Drawable snapMarker;
@@ -43,7 +54,7 @@ namespace osu.Game.Tests.Visual.Editor
                         RelativeSizeAxes = Axes.Both,
                         Alpha = 0.2f
                     },
-                    gridLayer = new GridLayer(),
+                    gridLayer = new TestGridLayer(),
                     snapMarker = new CircularContainer
                     {
                         Origin = Anchor.Centre,
@@ -57,7 +68,22 @@ namespace osu.Game.Tests.Visual.Editor
                     }
                 },
             };
+
+            gridLayer.ShowFor(null);
         });
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(6)]
+        [TestCase(8)]
+        [TestCase(12)]
+        [TestCase(16)]
+        public void TestBeatDivisor(int divisor)
+        {
+            AddStep("set beat divisor", () => BeatDivisor.Value = divisor);
+        }
 
         protected override bool OnMouseMove(MouseMoveEvent e)
         {
@@ -67,6 +93,21 @@ namespace osu.Game.Tests.Visual.Editor
             snapMarker.Position = gridLayer.ToLocalSpace(gridLayer.GetSnappedPosition(e.ScreenSpaceMousePosition));
 
             return true;
+        }
+
+        private class TestGridLayer : GridLayer
+        {
+            protected override IEnumerable<DrawableGrid> CreateGrids(IEnumerable<HitObject> hitObjects) => new TestDrawableCircularGrid().Yield();
+        }
+
+        private class TestDrawableCircularGrid : DrawableCircularGrid
+        {
+            public TestDrawableCircularGrid()
+                : base(new HitObject(), new Vector2(256), 25)
+            {
+            }
+
+            protected override float GetDistanceSpacing(double time, ControlPointInfo controlPointInfo, BeatmapDifficulty difficulty) => 0.05f;
         }
     }
 }
