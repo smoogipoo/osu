7b54bbad145f0b7ab543e4272e3ca8347666b36c
smoogipoo <smoogipoo@smgi.me>
2020-02-07 19:35:25 +0900

    Deselect removed items + prevent nullrefs

--- osu.Game/Screens/Multi/Match/Playlist.cs
+++ osu.Game/Screens/Multi/Match/Playlist.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Linq;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.IEnumerableExtensions;
 using osu.Framework.Graphics;
@@ -31,12 +32,18 @@ namespace osu.Game.Screens.Multi.Match
 
             SelectedItem.BindValueChanged(item =>
             {
-                if (item.OldValue != null)
-                    ((DrawablePlaylistItem)ItemMap[item.OldValue]).Deselect();
+                if (item.OldValue != null && ItemMap.TryGetValue(item.OldValue, out var oldItem))
+                    ((DrawablePlaylistItem)oldItem).Deselect();
 
-                if (item.NewValue != null)
-                    ((DrawablePlaylistItem)ItemMap[item.NewValue]).Select();
+                if (item.NewValue != null && ItemMap.TryGetValue(item.NewValue, out var newItem))
+                    ((DrawablePlaylistItem)newItem).Select();
             }, true);
+
+            Items.ItemsRemoved += items =>
+            {
+                if (items.Any(i => i == SelectedItem.Value))
+                    SelectedItem.Value = null;
+            };
         }
 
         protected override ScrollContainer<Drawable> CreateScrollContainer() => new OsuScrollContainer
@@ -57,24 +64,16 @@ namespace osu.Game.Screens.Multi.Match
             RequestDeletion = requestDeletion
         };
 
-        private void requestSelection(PlaylistItem item)
-        {
-            SelectedItem.Value = item;
-        }
+        private void requestSelection(PlaylistItem item) => SelectedItem.Value = item;
 
         private void requestDeletion(PlaylistItem item)
         {
-            if (Items.Count == 1)
-            {
-                // Don't allow deletion of the only item
-                return;
-            }
-
             if (SelectedItem.Value == item)
             {
-                // Attempt to select the next item in the list, falling back to the second item from the end.
-                // We are guaranteed to have at least 2 items here.
-                SelectedItem.Value = Items.GetNext(item) ?? Items[^2];
+                if (Items.Count == 1)
+                    SelectedItem.Value = null;
+                else
+                    SelectedItem.Value = Items.GetNext(item) ?? Items[^2];
             }
 
             Items.Remove(item);
