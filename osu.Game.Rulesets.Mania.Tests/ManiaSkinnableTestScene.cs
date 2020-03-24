5d8da26fc66188b5c77c5395a0d3dd4aebe39bc6
smoogipoo <smoogipoo@smgi.me>
2020-03-24 15:44:58 +0900

    Add background

--- osu.Game.Rulesets.Mania.Tests/ManiaSkinnableTestScene.cs
+++ osu.Game.Rulesets.Mania.Tests/ManiaSkinnableTestScene.cs
@@ -5,9 +5,13 @@ using System;
 using NUnit.Framework;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
 using osu.Game.Rulesets.UI.Scrolling;
 using osu.Game.Rulesets.UI.Scrolling.Algorithms;
 using osu.Game.Tests.Visual;
+using osuTK.Graphics;
 
 namespace osu.Game.Rulesets.Mania.Tests
 {
@@ -19,6 +23,13 @@ namespace osu.Game.Rulesets.Mania.Tests
         protected ManiaSkinnableTestScene()
         {
             scrollingInfo.Direction.Value = ScrollingDirection.Down;
+
+            Add(new Box
+            {
+                RelativeSizeAxes = Axes.Both,
+                Colour = Color4.SlateGray.Opacity(0.2f),
+                Depth = 1
+            });
         }
 
         [Test]
