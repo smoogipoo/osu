a07c9885acbe44b2f3dcea4b1f93accb8d453db7
smoogipoo <smoogipoo@smgi.me>
2020-10-21 18:51:51 +0900

    Fix test

--- osu.Game.Rulesets.Osu.Tests/TestSceneFollowPoints.cs
+++ osu.Game.Rulesets.Osu.Tests/TestSceneFollowPoints.cs
@@ -171,7 +171,7 @@ namespace osu.Game.Rulesets.Osu.Tests
                     }
 
                     hitObjectContainer.Add(drawableObject);
-                    followPointRenderer.AddFollowPoints(drawableObject);
+                    followPointRenderer.AddFollowPoints(objects[i]);
                 }
             });
         }
@@ -183,7 +183,9 @@ namespace osu.Game.Rulesets.Osu.Tests
                 var drawableObject = getFunc?.Invoke();
 
                 hitObjectContainer.Remove(drawableObject);
-                followPointRenderer.RemoveFollowPoints(drawableObject);
+
+                if (drawableObject != null)
+                    followPointRenderer.RemoveFollowPoints(drawableObject.HitObject);
             });
         }
 
@@ -215,10 +217,10 @@ namespace osu.Game.Rulesets.Osu.Tests
                     DrawableOsuHitObject expectedStart = getObject(i);
                     DrawableOsuHitObject expectedEnd = i < hitObjectContainer.Count - 1 ? getObject(i + 1) : null;
 
-                    if (getGroup(i).Start != expectedStart)
+                    if (getGroup(i).Start != expectedStart.HitObject)
                         throw new AssertionException($"Object {i} expected to be the start of group {i}.");
 
-                    if (getGroup(i).End != expectedEnd)
+                    if (getGroup(i).End != expectedEnd?.HitObject)
                         throw new AssertionException($"Object {(expectedEnd == null ? "null" : i.ToString())} expected to be the end of group {i}.");
                 }
 
