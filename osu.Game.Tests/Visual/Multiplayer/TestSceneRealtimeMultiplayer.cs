4f5d3d14e1ec526ed8169e9360ec9dc77c05fed8
smoogipoo <smoogipoo@smgi.me>
2020-12-09 19:53:07 +0900

    Add initial support for testing

--- osu.Game.Tests/Visual/Multiplayer/TestSceneRealtimeMultiplayer.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneRealtimeMultiplayer.cs
@@ -1,10 +1,11 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using osu.Framework.Allocation;
+using osu.Game.Online.API;
+using osu.Game.Online.Multiplayer;
 using osu.Game.Online.RealtimeMultiplayer;
-using osu.Game.Overlays;
 using osu.Game.Screens.Multi;
 using osu.Game.Screens.Multi.Realtime;
 
@@ -12,10 +13,7 @@ namespace osu.Game.Tests.Visual.Multiplayer
 {
     public class TestSceneRealtimeMultiplayer : ScreenTestScene
     {
-        protected override bool UseOnlineAPI => true;
-
-        [Cached]
-        private MusicController musicController { get; set; } = new MusicController();
+        private readonly List<Room> rooms = new List<Room>();
 
         public TestSceneRealtimeMultiplayer()
         {
@@ -23,6 +21,35 @@ namespace osu.Game.Tests.Visual.Multiplayer
 
             AddStep("show", () => LoadScreen(multi));
             AddUntilStep("wait for loaded", () => multi.IsLoaded);
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            ((DummyAPIAccess)API).HandleRequest = req =>
+            {
+                switch (req)
+                {
+                    case CreateRoomRequest createRoomRequest:
+                        var room = new APICreatedRoom { RoomID = { Value = 1 } };
+                        rooms.Add(room);
+                        createRoomRequest.TriggerSuccess(room);
+                        break;
+
+                    case JoinRoomRequest joinRoomRequest:
+                        joinRoomRequest.TriggerSuccess();
+                        break;
+
+                    case PartRoomRequest partRoomRequest:
+                        partRoomRequest.TriggerSuccess();
+                        break;
+
+                    case GetRoomsRequest getRoomsRequest:
+                        getRoomsRequest.TriggerSuccess(rooms);
+                        break;
+                }
+            };
         }
 
         private class TestRealtimeMultiplayer : RealtimeMultiplayer
