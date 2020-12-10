8861642a12c08d74586bde1abf5528d7c92b2689
smoogipoo <smoogipoo@smgi.me>
2020-12-11 00:29:48 +0900

    Make realtime multiplayer test scene use better test manager

--- osu.Game.Tests/Visual/Multiplayer/TestSceneRealtimeMultiplayer.cs
+++ osu.Game.Tests/Visual/Multiplayer/TestSceneRealtimeMultiplayer.cs
@@ -1,13 +1,9 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using osu.Framework.Allocation;
 using osu.Game.Online.API;
 using osu.Game.Online.Multiplayer;
-using osu.Game.Online.RealtimeMultiplayer;
 using osu.Game.Screens.Multi;
 using osu.Game.Screens.Multi.Realtime;
 
@@ -60,56 +56,7 @@ namespace osu.Game.Tests.Visual.Multiplayer
 
         private class TestRealtimeMultiplayer : RealtimeMultiplayer
         {
-            protected override IRoomManager CreateRoomManager() => new TestRealtimeRoomManager();
+            protected override IRoomManager CreateRoomManager() => new RealtimeMultiplayerTestScene.TestRoomManager();
         }
-
-        private class TestRealtimeRoomManager : RealtimeRoomManager
-        {
-            protected override Task Connect() => Task.CompletedTask;
-
-            protected override IStatefulMultiplayerClient CreateClient() => new TestRealtimeMultiplayerClient();
-        }
-
-#nullable enable
-
-        private class TestRealtimeMultiplayerClient : StatefulMultiplayerClient
-        {
-            public override MultiplayerRoom? Room => room;
-            private MultiplayerRoom? room;
-
-            [Resolved]
-            private IAPIProvider api { get; set; } = null!;
-
-            public override Task<MultiplayerRoom> JoinRoom(long roomId)
-            {
-                room = new MultiplayerRoom(roomId)
-                {
-                    Users = { new MultiplayerRoomUser(api.LocalUser.Value.Id) { User = api.LocalUser.Value } }
-                };
-
-                InvokeRoomChanged();
-
-                return Task.FromResult(room);
-            }
-
-            public override Task LeaveRoom()
-            {
-                room = null;
-
-                InvokeRoomChanged();
-
-                return Task.CompletedTask;
-            }
-
-            public override Task TransferHost(long userId) => throw new NotImplementedException();
-
-            public override Task ChangeSettings(MultiplayerRoomSettings settings) => throw new NotImplementedException();
-
-            public override Task ChangeState(MultiplayerUserState newState) => throw new NotImplementedException();
-
-            public override Task StartMatch() => throw new NotImplementedException();
-        }
-
-#nullable disable
     }
 }
