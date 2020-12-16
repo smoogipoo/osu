38de1f6e6843a7c0a880575c42f9df116367d13b
smoogipoo <smoogipoo@smgi.me>
2020-12-16 22:56:24 +0900

    Return to idle state upon completion

--- osu.Game/Screens/Multi/Realtime/RealtimeMultiplayer.cs
+++ osu.Game/Screens/Multi/Realtime/RealtimeMultiplayer.cs
@@ -3,7 +3,9 @@
 
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
+using osu.Framework.Screens;
 using osu.Game.Online.Multiplayer;
+using osu.Game.Online.RealtimeMultiplayer;
 using osu.Game.Screens.Multi.Lounge;
 
 namespace osu.Game.Screens.Multi.Realtime
@@ -14,6 +16,17 @@ namespace osu.Game.Screens.Multi.Realtime
 
         [Cached(typeof(RealtimeRoomManager))]
         protected override IRoomManager RoomManager => base.RoomManager;
+
+        [Resolved]
+        private StatefulMultiplayerClient client { get; set; }
+
+        public override void OnResuming(IScreen last)
+        {
+            base.OnResuming(last);
+
+            if (client.Room != null)
+                client.ChangeState(MultiplayerUserState.Idle);
+        }
 
         protected override IRoomManager CreateRoomManager() => new RealtimeRoomManager();
 
