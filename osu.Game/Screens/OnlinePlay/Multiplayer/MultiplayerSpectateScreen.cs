c895c1cb8fd1ca7374d87156bf6b96a31f6e2352
smoogipoo <smoogipoo@smgi.me>
2021-03-03 21:38:00 +0900

    Beginnings of scroll + better cell layout

--- osu.Game/Screens/OnlinePlay/Multiplayer/MultiplayerSpectateScreen.cs
+++ osu.Game/Screens/OnlinePlay/Multiplayer/MultiplayerSpectateScreen.cs
@@ -1,19 +1,17 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using osu.Framework.Allocation;
-using osu.Framework.Bindables;
 using osu.Framework.Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Game.Beatmaps;
 using osu.Game.Database;
+using osu.Game.Graphics.Containers;
 using osu.Game.Online.Rooms;
-using osu.Game.Rulesets;
-using osu.Game.Rulesets.Mods;
 using osu.Game.Screens.Play;
+using osu.Game.Users;
 using osuTK;
 
 namespace osu.Game.Screens.OnlinePlay.Multiplayer
@@ -22,11 +20,16 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer
     {
         // Isolates beatmap/ruleset to this screen.
         public override bool DisallowExternalBeatmapRulesetChanges => true;
+
+        private readonly List<Drawable> instances = new List<Drawable>();
+
         private readonly PlaylistItem playlistItem;
         private readonly int[] userIds;
 
         [Resolved]
         private UserLookupCache userLookupCache { get; set; }
+
+        private GridContainer grid;
 
         public MultiplayerSpectateScreen(PlaylistItem playlistItem, int[] userIds)
         {
@@ -37,43 +40,49 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer
         [BackgroundDependencyLoader]
         private void load(UserLookupCache userLookupCache)
         {
-            Drawable[,] instances = new Drawable[4, 4];
+            instances.AddRange(userIds.Select(u => new PlayerCell(userLookupCache.GetUserAsync(u).Result)));
 
-            for (int x = 0; x < instances.GetLength(0); x++)
-            {
-                for (int y = 0; y < instances.GetLength(1); y++)
-                    instances[x, y] = createInstance(userIds[0]);
-            }
-
-            InternalChild = new GridContainer
+            InternalChild = new OsuScrollContainer
             {
                 RelativeSizeAxes = Axes.Both,
-                Content = instances.ToJagged()
+                Child = grid = new GridContainer
+                {
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y
+                }
             };
+
+            recreateGrid();
         }
 
-        private Drawable createInstance(int userId)
+        private void recreateGrid()
         {
-            var stack = new ScalingScreenStack();
-            var user = userLookupCache.GetUserAsync(userId).Result;
-            stack.Push(new Spectator(user));
+            // Todo:
+            var cells = new Drawable[4, 4];
 
-            return stack;
+            for (int x = 0; x < cells.GetLength(0); x++)
+            {
+                for (int y = 0; y < cells.GetLength(1); y++)
+                    cells[x, y] = instances[y * cells.GetLength(1) + x];
+            }
+
+            grid.Content = cells.ToJagged();
         }
 
-        private class ScalingScreenStack : OsuScreenStack
+        private class PlayerCell : OsuScreenStack
         {
-            public ScalingScreenStack()
+            public PlayerCell(User user)
             {
                 RelativeSizeAxes = Axes.None;
+                Push(new Spectator(user));
             }
 
             protected override void Update()
             {
                 base.Update();
 
-                Size = Parent.Parent.DrawSize;
-                Scale = Vector2.Divide(Parent.DrawSize, Size);
+                Size = Parent.Parent.ChildSize;
+                Scale = Vector2.Divide(Parent.ChildSize, Size);
             }
         }
     }
