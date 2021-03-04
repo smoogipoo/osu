e65ee94ca3716fc101d1789b8ae2c87faa5e50ff
smoogipoo <smoogipoo@smgi.me>
2021-03-04 12:56:25 +0900

    Rework grid and layout

--- osu.Game/Screens/OnlinePlay/Multiplayer/MultiplayerSpectateScreen.cs
+++ osu.Game/Screens/OnlinePlay/Multiplayer/MultiplayerSpectateScreen.cs
@@ -2,9 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System.Collections.Generic;
-using System.Linq;
 using osu.Framework.Allocation;
-using osu.Framework.Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Database;
@@ -18,10 +16,10 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer
 {
     public class MultiplayerSpectateScreen : OsuScreen
     {
+        private const float player_spacing = 10;
+
         // Isolates beatmap/ruleset to this screen.
         public override bool DisallowExternalBeatmapRulesetChanges => true;
-
-        private readonly List<Drawable> instances = new List<Drawable>();
 
         private readonly PlaylistItem playlistItem;
         private readonly int[] userIds;
@@ -29,7 +27,10 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer
         [Resolved]
         private UserLookupCache userLookupCache { get; set; }
 
-        private GridContainer grid;
+        private readonly List<Drawable> players = new List<Drawable>();
+
+        private OsuScrollContainer scroll;
+        private FillFlowContainer<PlayerFacade> flow;
 
         public MultiplayerSpectateScreen(PlaylistItem playlistItem, int[] userIds)
         {
@@ -40,40 +41,115 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer
         [BackgroundDependencyLoader]
         private void load(UserLookupCache userLookupCache)
         {
-            instances.AddRange(userIds.Select(u => new PlayerCell(userLookupCache.GetUserAsync(u).Result)));
-
-            InternalChild = new OsuScrollContainer
+            InternalChild = scroll = new OsuScrollContainer
             {
                 RelativeSizeAxes = Axes.Both,
-                Child = grid = new GridContainer
+                Child = flow = new FillFlowContainer<PlayerFacade>
                 {
                     RelativeSizeAxes = Axes.X,
-                    AutoSizeAxes = Axes.Y
+                    AutoSizeAxes = Axes.Y,
+                    Padding = new MarginPadding(player_spacing),
+                    Spacing = new Vector2(player_spacing),
                 }
             };
 
-            recreateGrid();
+            for (int i = 0; i < 128; i++)
+            {
+                var facade = new PlayerFacade();
+                var player = new PlayerCell(userLookupCache.GetUserAsync(userIds[0]).Result, facade) { Depth = 1 };
+
+                flow.Add(facade);
+                players.Add(player);
+
+                AddInternal(player);
+            }
         }
 
-        private void recreateGrid()
+        protected override void Update()
         {
-            // Todo:
-            var cells = new Drawable[4, 4];
+            base.Update();
 
-            for (int x = 0; x < cells.GetLength(0); x++)
+            Vector2 cellsPerDimension;
+
+            switch (flow.Count)
             {
-                for (int y = 0; y < cells.GetLength(1); y++)
-                    cells[x, y] = instances[y * cells.GetLength(1) + x];
+                case 1:
+                    cellsPerDimension = Vector2.One;
+                    break;
+
+                case 2:
+                    cellsPerDimension = new Vector2(2, 1);
+                    break;
+
+                case 3:
+                case 4:
+                    cellsPerDimension = new Vector2(2);
+                    break;
+
+                case 5:
+                case 6:
+                    cellsPerDimension = new Vector2(3, 2);
+                    break;
+
+                case 7:
+                case 8:
+                case 9:
+                    // 3 rows / 3 cols.
+                    cellsPerDimension = new Vector2(3);
+                    break;
+
+                case 10:
+                case 11:
+                case 12:
+                    // 3 rows / 4 cols.
+                    cellsPerDimension = new Vector2(4, 3);
+                    break;
+
+                default:
+                    // 4 rows / 4 cols.
+                    cellsPerDimension = new Vector2(4);
+                    break;
             }
 
-            grid.Content = cells.ToJagged();
+            Vector2 totalSpacing = player_spacing * (cellsPerDimension - Vector2.One);
+
+            Vector2 fullSize = new Vector2(flow.ChildSize.X, scroll.ChildSize.Y) - totalSpacing;
+            Vector2 cellSize = Vector2.Divide(fullSize, new Vector2(cellsPerDimension.X, cellsPerDimension.Y));
+
+            foreach (var facade in flow)
+            {
+                facade.Size = cellSize;
+                facade.FullSize = fullSize;
+            }
+        }
+
+        private class PlayerFacade : Drawable
+        {
+            /// <summary>
+            /// The size of the entire screen area.
+            /// </summary>
+            public Vector2 FullSize;
+
+            public PlayerFacade()
+            {
+                Anchor = Anchor.Centre;
+                Origin = Anchor.Centre;
+            }
         }
 
         private class PlayerCell : OsuScreenStack
         {
-            public PlayerCell(User user)
+            private readonly PlayerFacade facade;
+
+            public PlayerCell(User user, PlayerFacade facade)
             {
+                this.facade = facade;
+
                 RelativeSizeAxes = Axes.None;
+                Origin = Anchor.Centre;
+
+                Masking = true;
+
                 Push(new Spectator(user));
             }
 
@@ -81,8 +157,11 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer
             {
                 base.Update();
 
-                Size = Parent.Parent.ChildSize;
-                Scale = Vector2.Divide(Parent.ChildSize, Size);
+                var topLeft = Parent.ToLocalSpace(facade.ToScreenSpace(Vector2.Zero));
+                Position = topLeft + facade.DrawSize / 2;
+
+                Size = facade.FullSize;
+                Scale = Vector2.Divide(facade.DrawSize, Size);
             }
         }
     }
