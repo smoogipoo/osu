490bdf4bcd0e81fe6967b01428aa5801b9894a05
Dan Balasescu <smoogipoo@smgi.me>
2022-01-17 16:19:05 +0900

    Revert "Merge pull request #15867 from MBmasher/fl-cumulative-strain"
    This reverts commit 05b79f864e20823596dc37c29554181d206a908d, reversing
    changes made to a53c67b2ea8f04f3e47129195c2edc5b0886bad8.

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Flashlight.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Flashlight.cs
@@ -40,31 +40,26 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
             double result = 0.0;
 
-            OsuDifficultyHitObject lastObj = osuCurrent;
-
-            // This is iterating backwards in time from the current object.
             for (int i = 0; i < Previous.Count; i++)
             {
-                var currentObj = (OsuDifficultyHitObject)Previous[i];
-                var currentHitObject = (OsuHitObject)(currentObj.BaseObject);
+                var osuPrevious = (OsuDifficultyHitObject)Previous[i];
+                var osuPreviousHitObject = (OsuHitObject)(osuPrevious.BaseObject);
 
-                if (!(currentObj.BaseObject is Spinner))
+                if (!(osuPrevious.BaseObject is Spinner))
                 {
-                    double jumpDistance = (osuHitObject.StackedPosition - currentHitObject.EndPosition).Length;
+                    double jumpDistance = (osuHitObject.StackedPosition - osuPreviousHitObject.EndPosition).Length;
 
-                    cumulativeStrainTime += lastObj.StrainTime;
+                    cumulativeStrainTime += osuPrevious.StrainTime;
 
                     // We want to nerf objects that can be easily seen within the Flashlight circle radius.
                     if (i == 0)
                         smallDistNerf = Math.Min(1.0, jumpDistance / 75.0);
 
                     // We also want to nerf stacks so that only the first object of the stack is accounted for.
-                    double stackNerf = Math.Min(1.0, (currentObj.LazyJumpDistance / scalingFactor) / 25.0);
+                    double stackNerf = Math.Min(1.0, (osuPrevious.LazyJumpDistance / scalingFactor) / 25.0);
 
                     result += Math.Pow(0.8, i) * stackNerf * scalingFactor * jumpDistance / cumulativeStrainTime;
                 }
-
-                lastObj = currentObj;
             }
 
             return Math.Pow(smallDistNerf * result, 2.0);
