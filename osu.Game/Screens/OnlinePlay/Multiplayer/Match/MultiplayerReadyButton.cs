09ed27cb180d22c666d30077ffa3fba3a4ded074
Dan Balasescu <smoogipoo@smgi.me>
2022-02-28 17:01:57 +0900

    Move ready button implementation to the button

--- osu.Game/Screens/OnlinePlay/Multiplayer/Match/MultiplayerReadyButton.cs
+++ osu.Game/Screens/OnlinePlay/Multiplayer/Match/MultiplayerReadyButton.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Linq;
 using Humanizer;
+using JetBrains.Annotations;
 using osu.Framework.Allocation;
 using osu.Framework.Audio;
 using osu.Framework.Audio.Sample;
@@ -21,9 +22,7 @@ using osu.Game.Graphics.Backgrounds;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Graphics.UserInterfaceV2;
 using osu.Game.Online.Multiplayer;
-using osu.Game.Screens.OnlinePlay.Components;
 using osuTK;
-using osuTK.Graphics;
 
 namespace osu.Game.Screens.OnlinePlay.Multiplayer.Match
 {
@@ -31,19 +30,10 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer.Match
     {
         public Action<TimeSpan?> OnReadyClick;
 
-        [Resolved]
-        private OsuColour colours { get; set; }
-
-        [Resolved]
-        private OngoingOperationTracker ongoingOperationTracker { get; set; }
-
-        private IBindable<bool> operationInProgress;
-
         private Sample sampleReady;
         private Sample sampleReadyAll;
         private Sample sampleUnready;
 
-        private readonly ButtonWithTrianglesExposed mainButton;
         private readonly CountdownButton countdownButton;
 
         private int countReady;
@@ -64,7 +54,7 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer.Match
                 {
                     new Drawable[]
                     {
-                        mainButton = new ButtonWithTrianglesExposed
+                        new ReadyButton
                         {
                             RelativeSizeAxes = Axes.Both,
                             Size = Vector2.One,
@@ -88,9 +78,6 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer.Match
         [BackgroundDependencyLoader]
         private void load(AudioManager audio)
         {
-            operationInProgress = ongoingOperationTracker.InProgress.GetBoundCopy();
-            operationInProgress.BindValueChanged(_ => updateState());
-
             sampleReady = audio.Samples.Get(@"Multiplayer/player-ready");
             sampleReadyAll = audio.Samples.Get(@"Multiplayer/player-ready-all");
             sampleUnready = audio.Samples.Get(@"Multiplayer/player-unready");
@@ -121,43 +108,13 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer.Match
             {
                 default:
                     countdownButton.Alpha = 0;
-                    mainButton.Text = "Ready";
-                    updateButtonColour(true);
                     break;
 
                 case MultiplayerUserState.Spectating:
                 case MultiplayerUserState.Ready:
-
-                    string countText = $"({newCountReady} / {newCountTotal} ready)";
-
-                    if (Room?.Host?.Equals(localUser) == true)
-                    {
-                        countdownButton.Alpha = 1;
-                        mainButton.Text = $"Start match {countText}";
-                        updateButtonColour(true);
-                    }
-                    else
-                    {
-                        countdownButton.Alpha = 0;
-                        mainButton.Text = $"Waiting for host... {countText}";
-                        updateButtonColour(false);
-                    }
-
+                    countdownButton.Alpha = Room?.Host?.Equals(localUser) == true ? 1 : 0;
                     break;
             }
-
-            bool enableButton =
-                Room?.State == MultiplayerRoomState.Open
-                && CurrentPlaylistItem.Value?.ID == Room.Settings.PlaylistItemId
-                && !Room.Playlist.Single(i => i.ID == Room.Settings.PlaylistItemId).Expired
-                && !operationInProgress.Value;
-
-            // When the local user is the host and spectating the match, the "start match" state should be enabled if any users are ready.
-            if (localUser?.State == MultiplayerUserState.Spectating)
-                enableButton &= Room?.Host?.Equals(localUser) == true && newCountReady > 0;
-
-            mainButton.Enabled.Value = enableButton;
-            countdownButton.Enabled.Value = enableButton;
 
             if (newCountReady == countReady)
                 return;
@@ -181,38 +138,103 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer.Match
             });
         }
 
-        private void updateButtonColour(bool green)
-        {
-            if (green)
-            {
-                countdownButton.BackgroundColour = colours.Green;
-                mainButton.BackgroundColour = colours.Green;
-                mainButton.Triangles.ColourDark = colours.Green;
-                mainButton.Triangles.ColourLight = colours.GreenLight;
-            }
-            else
-            {
-                countdownButton.BackgroundColour = colours.YellowDark;
-                mainButton.BackgroundColour = colours.YellowDark;
-                mainButton.Triangles.ColourDark = colours.YellowDark;
-                mainButton.Triangles.ColourLight = colours.Yellow;
-            }
-        }
-
-        private class ButtonWithTrianglesExposed : ReadyButton
+        private class ReadyButton : Components.ReadyButton
         {
             public new Triangles Triangles => base.Triangles;
+
+            [Resolved]
+            private MultiplayerClient multiplayerClient { get; set; }
+
+            [Resolved]
+            private OsuColour colours { get; set; }
+
+            [Resolved]
+            private OngoingOperationTracker ongoingOperationTracker { get; set; }
+
+            [CanBeNull]
+            private MultiplayerRoom room => multiplayerClient.Room;
+
+            private IBindable<bool> operationInProgress;
+
+            [BackgroundDependencyLoader]
+            private void load()
+            {
+                operationInProgress = ongoingOperationTracker.InProgress.GetBoundCopy();
+                operationInProgress.BindValueChanged(_ => onRoomUpdated());
+            }
+
+            protected override void LoadComplete()
+            {
+                base.LoadComplete();
+
+                multiplayerClient.RoomUpdated += onRoomUpdated;
+                onRoomUpdated();
+            }
+
+            private void onRoomUpdated()
+            {
+                var localUser = multiplayerClient.LocalUser;
+
+                int newCountReady = room?.Users.Count(u => u.State == MultiplayerUserState.Ready) ?? 0;
+                int newCountTotal = room?.Users.Count(u => u.State != MultiplayerUserState.Spectating) ?? 0;
+
+                switch (localUser?.State)
+                {
+                    default:
+                        Text = "Ready";
+                        BackgroundColour = colours.Green;
+                        Triangles.ColourDark = colours.Green;
+                        Triangles.ColourLight = colours.GreenLight;
+                        break;
+
+                    case MultiplayerUserState.Spectating:
+                    case MultiplayerUserState.Ready:
+
+                        string countText = $"({newCountReady} / {newCountTotal} ready)";
+
+                        if (room?.Host?.Equals(localUser) == true)
+                        {
+                            Text = $"Start match {countText}";
+                            BackgroundColour = colours.Green;
+                            Triangles.ColourDark = colours.Green;
+                            Triangles.ColourLight = colours.GreenLight;
+                        }
+                        else
+                        {
+                            Text = $"Waiting for host... {countText}";
+                            BackgroundColour = colours.YellowDark;
+                            Triangles.ColourDark = colours.YellowDark;
+                            Triangles.ColourLight = colours.Yellow;
+                        }
+
+                        break;
+                }
+
+                bool enableButton =
+                    room?.State == MultiplayerRoomState.Open
+                    // TODO: && CurrentPlaylistItem.Value?.ID == Room.Settings.PlaylistItemId
+                    && !room.Playlist.Single(i => i.ID == room.Settings.PlaylistItemId).Expired
+                    && !operationInProgress.Value;
+
+                // When the local user is the host and spectating the match, the "start match" state should be enabled if any users are ready.
+                if (localUser?.State == MultiplayerUserState.Spectating)
+                    enableButton &= room?.Host?.Equals(localUser) == true && newCountReady > 0;
+
+                Enabled.Value = enableButton;
+            }
+
+            protected override void Dispose(bool isDisposing)
+            {
+                base.Dispose(isDisposing);
+
+                if (multiplayerClient != null)
+                    multiplayerClient.RoomUpdated -= onRoomUpdated;
+            }
         }
 
         public class CountdownButton : IconButton, IHasPopover
         {
             public new Action<TimeSpan> Action;
-
-            public Color4 BackgroundColour
-            {
-                get => background.Colour;
-                set => background.Colour = value;
-            }
 
             private readonly Drawable background;
 
@@ -225,6 +247,12 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer.Match
                 });
 
                 base.Action = this.ShowPopover;
+            }
+
+            [BackgroundDependencyLoader]
+            private void load(OsuColour colours)
+            {
+                background.Colour = colours.Green;
             }
 
             private static readonly TimeSpan[] available_delays =
@@ -251,7 +279,7 @@ namespace osu.Game.Screens.OnlinePlay.Multiplayer.Match
                     {
                         RelativeSizeAxes = Axes.X,
                         Text = $"Start match in {duration.Humanize()}",
-                        BackgroundColour = BackgroundColour,
+                        BackgroundColour = background.Colour,
                         Action = () =>
                         {
                             Action(duration);
