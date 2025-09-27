0ee65c4b435e571e322c46c363b7754bdf4d470f
Dan Balasescu <smoogipoo@smgi.me>
2025-09-27 16:43:07 +0900

    Fix options footer button not working

--- osu.Game/Screens/SelectV2/SongSelect.cs
+++ osu.Game/Screens/SelectV2/SongSelect.cs
@@ -349,7 +349,7 @@ namespace osu.Game.Screens.SelectV2
                         errorSample?.Play();
                 }
             },
-            new FooterButtonOptions
+            new FooterButtonOptions(this)
             {
                 Hotkey = GlobalAction.ToggleBeatmapOptions,
             }
