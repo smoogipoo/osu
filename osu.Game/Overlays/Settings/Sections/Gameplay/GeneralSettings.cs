c0c5bde87d222ddfc83e1bb62522b70720f71b6b
Dan Balasescu <smoogipoo@smgi.me>
2026-07-24 23:01:06 +0900

    Remove unused using

--- osu.Game/Overlays/Settings/Sections/Gameplay/GeneralSettings.cs
+++ osu.Game/Overlays/Settings/Sections/Gameplay/GeneralSettings.cs
@@ -2,7 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
-using osu.Framework.Graphics;
 using osu.Framework.Graphics.UserInterface;
 using osu.Framework.Localisation;
 using osu.Game.Configuration;
